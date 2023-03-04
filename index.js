22a2d9c8635adf9a894c77084d2e533b82288eef
nlai <nicholaslhlai@gmail.com>
2023-03-03 23:49:55 -0500

    Add functionality to reset grid when clicking new grid

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function createGrid(x) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
+function createGrid(numGrids) {
+    for (var rows = 0; rows < numGrids; rows++) {
+        for (var columns = 0; columns < numGrids; columns++) {
             let grid = document.createElement("div");
             grid.className = "grid";
             document.querySelector(".container").appendChild(grid);
@@ -8,11 +8,16 @@ function createGrid(x) {
     };
     let grids = document.getElementsByClassName("grid");
     for (let i = 0; i < grids.length; i++){
-        grids[i].style.width = String(800/x) + "px";
-        grids[i].style.height = String(800/x) + "px";
+        grids[i].style.width = String(800/numGrids) + "px";
+        grids[i].style.height = String(800/numGrids) + "px";
     }
 
-}; 
+};
+
+function removeGrid() {
+    let container = document.querySelector(".container");
+    container.innerHTML = '';
+}
 
 function draw(e) {
     e.target.style.backgroundColor = "black";
@@ -20,6 +25,12 @@ function draw(e) {
 
 function newGrid(e) {
     let numGrid = window.prompt("How many squares per side would you like? (100 Maximum)");
+    removeGrid();
+
+    while (numGrid > 100) {
+        numGrid = window.prompt("Invalid Selection. The maximum number of grids per side is 100. How many squares per side would you like?");
+    } 
+
     createGrid(numGrid);
 
     let gridsList = document.getElementsByClassName("grid");
